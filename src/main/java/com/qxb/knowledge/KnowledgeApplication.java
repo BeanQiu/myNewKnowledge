512fe1f9386caaab40249f7132c3124897fab7b1
Xiaobin <seven_qxb@163.com>
2020-04-13 16:17:16 +0800

    基础配置

---
+++ src/main/java/com/qxb/knowledge/KnowledgeApplication.java
@@ -0,0 +1,15 @@
+package com.qxb.knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Xiaobin Qiu
+ * @describe 项目启动入口
+ */
+@SpringBootApplication
+public class KnowledgeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeApplication.class, args);
+    }
+}
